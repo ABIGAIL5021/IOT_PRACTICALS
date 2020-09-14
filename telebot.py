48477d0905cc76719f4e33c46ebee1130f331fb9
abiewilson23 <71007208+abiewilson23@users.noreply.github.com>
2020-09-14 11:07:21 +0530

    created telegrambot code

--- telebot.py
+++ telebot.py
@@ -2,3 +2,11 @@ import telepot
 token='1302764251:AAFK-t2r9xGxy0a0O5OcLs_5H7akNNuwHg'
 TelegramBot=telepot.Bot(token)
 print(TelegramBot.getMe())
+def handle(msg):
+  content_type,chat_type,chat_id=telepot.galnce(msg)
+  print(content_type,chat_type,chat_id)
+  
+  if content_type=='text':
+    TelegramBot.sendMessage(chat_id,"you said'{}'".format(msg["text"]))
+    
+TelegramBot.message_loop(handle)
