7834fe9a2e2ae40093b34ce54f9e3cccda37601e
ABIGAIL5021 <71007208+ABIGAIL5021@users.noreply.github.com>
2020-09-16 14:18:27 +0530

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -1,5 +1,5 @@
 import telepot
-token='1302764251:AAFK-t2r9xGxy0a0O5OcLs_5H7akNNuwHg'
+token='your token from botfather'
 TelegramBot=telepot.Bot(token)
 print(TelegramBot.getMe())
 def handle(msg):
